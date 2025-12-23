4fb527448c50a74340f367cdd368d922460ffca5
Mahadeen <mahadeenabd@gmail.com>
2025-12-23 09:55:37 +0300

    finished product.

---
+++ README.md
@@ -0,0 +1,14 @@
+Order Analyzer
+A Python tool to calculate restaurant performance metrics from JSON data.
+
+Usage
+Run the script by passing the path to your JSON file:
+
+Bash
+python order_analysis.py orders.json
+The script will generate an output.json file containing the processed report.
+
+Key Metrics
+Average order value and hourly revenue, completion rate and daily order frequency, top and bottom performing restaurants by revenue.
+
+Environment: Built for Python3
--- order_analysis.py
+++ order_analysis.py
@@ -27,57 +27,97 @@ def main():
     output_file.write(json_report)
     output_file.close()
 
-
-
-
-
 def get_report(orders):
     result = {
         'count': orders_count(orders),
-        'avg_order_value': avg_order_value(orders)
+        'avg order value': avg_order_value(orders),
+        'avg daily order count': avg_daily_order_count(orders),
+        'avg hourly revenue': avg_hourly_revenue(orders),
+        'completion rate': completion_rate(orders),
+        'top_2_restaurants': top_performing_restaurants(orders),
+        'least_2_restaurants': least_performing_restaurants(orders),
     }
     return result
-
 
 def orders_count (orders):
     count = len(orders)
     return count
 
 def avg_order_value (orders):
+    if not orders:
+        return 0
+    # get avg total value of orders
     total = sum(order['total'] for order in orders)
+    # get number of orders
     count = orders_count(orders)
+    # divide total value by number of orders
     avg = total / count
-    return avg
+    return round(avg, 2)
 
-# def mean_daily_order_count (request):
-#     # orders_count () devided by 7 (days)
-#     # return value
-#     return
+def avg_daily_order_count (orders):
+    # get total number of orders
+    count = orders_count (orders)
+    # devide count by 7 days (assuming that it's a weekly report)
+    daily_avg_count = count / 7
+    return round(daily_avg_count, 2)
 
-# def mean_hourly_revenue (request):
-#     # mean_order_value () devided by 24 hours
-#     # return value
-#     return
+def avg_hourly_revenue (orders):
+    # get total orders value
+    total = sum(order['total'] for order in orders)
+    # devide total by 168 (24 hours * 7 days) (assuming that it's a weekly report)
+    avg_hourly_revenue = total / 168
+    return round(avg_hourly_revenue, 2)
 
-# def top_performing_restaurants(request):
-#     # retrieve all orders
-#     # match them by restaurants
-#     # get the "max" value of orders for the top 5 restaurants
-#     # return values
-#     return
+def completion_rate(orders):
+    if not orders:
+        return "0 %"
+    # count completed orders by iterating through dict
+    count = 0
+    for order in orders:
+        value = order['status']
+        if value == "completed":
+            count = count + 1
+    # calculate percentage
+    percentage = count / orders_count(orders)
+    return f'{int(percentage * 100)} %'
+        
+def group_values (orders):
+    # create an empty array
+    grouped = {}
+    # iterate through the dict
+    for order in orders:
+        res_id = order['restaurant_id']
+        res_name = order ['restaurant_name']
+        total = order ['total']
 
-# def least_performing_restaurants(request):
-#     # retrieve all orders
-#     # match them by restaurants
-#     # get the "min" value of orders for the top 5 restaurants
-#     # return values
-#     return
+        # check if the restaurant doesn't exist in the new array
+        if res_id not in grouped:
+            grouped[res_id] = {
+                'restaurant_id': res_id,
+                'restaurant_name': res_name,
+                'total_revenue': total,
+                'orders_count': 1,
+            }
+        else:
+            # append only the value to that
+            grouped[res_id]['total_revenue'] += total
+            grouped[res_id]['orders_count'] += 1
 
-# def completion_rate(request):
-#     # retrieve all orders
-#     # match them by statuses
-#     # return value
-#     return
+    return list(grouped.values())
+
+def by_total_revenue(item):
+    return item['total_revenue']
+
+def top_performing_restaurants(orders):
+    grouped = group_values(orders)
+    grouped.sort(key=by_total_revenue, reverse=True)
+    return grouped[:2]
+
+
+def least_performing_restaurants(orders):
+    grouped = group_values(orders)
+    grouped.sort(key=by_total_revenue)
+    return grouped[:2]
 
 if __name__ == "__main__":
     main()
